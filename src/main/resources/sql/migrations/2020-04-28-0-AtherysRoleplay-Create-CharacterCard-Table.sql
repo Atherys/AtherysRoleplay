cfb0c74da22391b7ae3dabfd1dbc0cdcba08b674
HaedHutner <mvvasilev@outlook.com>
2020-04-28 20:22:28 +0300

    Add charactercard DDL

---
+++ src/main/resources/sql/migrations/2020-04-28-0-AtherysRoleplay-Create-CharacterCard-Table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE atherys.charactercard (
+    player uuid NOT NULL,
+    age character varying(255),
+    description character varying(255),
+    name character varying(255),
+    nationality character varying(255),
+    nickname character varying(255),
+    playername character varying(255)
+);
